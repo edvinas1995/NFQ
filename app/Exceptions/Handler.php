737cb66e3ea842fda65f8ca4a07fad54bc9ee836
Edvinas  Saltenis <edvinassaltenis@Edvinass-MBP.lan>
2017-09-18 01:46:56 +0300

    err

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,8 +44,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if (!$this->isHttpException($e)) $e = new \Symfony\Component\HttpKernel\Exception\HttpException(500);
-
         return parent::render($request, $e);
     }
 
